af31d301cd0999af73799a4a7e6780ab38735f17
unknown <3933714051@bitraipur.ac.in>
2022-04-15 15:14:59 +0530

    added the favourite functionallity and state and tab feature

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,5 +1,8 @@
 //action types
 export const ADD_MOVIES = "ADD_MOVIES";
+export const ADD_FAVOURITE = "ADD_FAVOURITE";
+export const REMOVE_FAVOURITE = "REMOVE_FAVOURITE";
+export const SET_SHOW_FAVOURITES = "SET_SHOW_FAVOURITES";
 
 //action creators
 export function addMovies(movies) {
@@ -7,5 +10,26 @@ export function addMovies(movies) {
   return {
     type: ADD_MOVIES,
     movies,
+  };
+}
+
+export function addFavourite(movie) {
+  return {
+    type: ADD_FAVOURITE,
+    movie,
+  };
+}
+
+export function removeFavourite(movie) {
+  return {
+    type: REMOVE_FAVOURITE,
+    movie,
+  };
+}
+
+export function setShowFavourite(val) {
+  return {
+    type: SET_SHOW_FAVOURITES,
+    val,
   };
 }
